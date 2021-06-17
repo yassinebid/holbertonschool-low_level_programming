bdd96cd4164c453e09d5a7e9c8508863f7f1a496
yasinebid <3237@holbertonschool.com>
2021-06-17 15:33:59 -0700

    try

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-/* new c */
+/**
+ * main - Prints the size of various types based on
+ *        the computer it is compiled and run on..
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
