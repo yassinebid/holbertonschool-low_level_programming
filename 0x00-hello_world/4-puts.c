7cac5d9b6f079dd7db7f0921814ea60591eebc77
yasinebid <3237@holbertonschool.com>
2021-06-17 04:03:24 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(void)
+//new c
 {
- puts("\"Programming is like building a multilingual puzzle");
- return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
-
