0166fc8ce7ac19cd7704a7a2aca3dd4bcbf2d3ef
yasinebid <3237@holbertonschool.com>
2021-06-23 15:33:42 -0700

    error

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,14 +1,15 @@
 #include "holberton.h"
 
 /**
- * _islower - Checks if a character is lowercase.
- * 
+ * _isalpha - Checks if a character is alphabetic.
+ * @c: The character to be checked.
  *
- * Return: 1 if character is lowercase, 0 otherwise.
+ * Return: 1 if character is letter, lowercase or uppercase, 0 otherwise.
  */
-int _islower(int c)
+int _isalpha(int c)
 {
-	if (c >= 'a' && c <= 'z')
+	if ((c >= 'a' && c <= 'z') ||
+	    (c >= 'A' && c <= 'Z'))
 		return (1);
 	else
 		return (0);
--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 
 /**
  * print_sign - Prints the sign of a number.
- * 
+ * @n: The number of which the sign will be printed.
  *
  * Return: 1 if the number is greater than zero,
  *         0 if the number is zero,
