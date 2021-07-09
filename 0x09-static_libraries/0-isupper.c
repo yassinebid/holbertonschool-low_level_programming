ed43d3837763cba90325b084e372079c89b59359
yasinebid <3237@holbertonschool.com>
2021-07-09 15:18:41 -0700

    this is real

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,7 @@
+#include "holberton.h"
+
+int _isupper(int c)
+{
+(void)c;
+return (0);
+}
---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,9 @@
+#include "holberton.h"
+
+char *_memset(char *s, char b, unsigned int n)
+{
+  (void)s;
+  (void)b;
+  (void)n;
+  return s;
+}
