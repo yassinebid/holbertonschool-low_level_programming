92836b19a08ad337b03f7588d24628b8a24d9472
yasinebid <3237@holbertonschool.com>
2021-09-16 13:06:14 -0700

    not py

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,22 @@
+#include lists_h
+
+/**
+ * print_dlistint - Prints all the elements of a dlistint_t list.
+ * @h: The head of the dlistint_t list.
+ *
+ * Return: The number of nodes in the list.
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+  	size_t nod = 0;
+
+	while (h)
+	{
+		nod++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+
+	return (nod);
+}
