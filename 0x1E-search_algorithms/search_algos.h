7bc9148a760f243a4ab86e8a592747720204cd7e
yassinebid <3237@holbertonschool.com>
2022-04-06 18:11:20 +0100

    fix

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,12 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+
+#endif
